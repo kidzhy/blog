ee37a353e1afe694436f4c37d614d462a6727cfa
zhy <zhyznj@sina.com>
2017-02-07 15:53:23 +0800

    增加servicework

---
+++ sw.js
@@ -0,0 +1,37 @@
+var cacheName = 'kidzhy-blog'
+
+var filesCache = [
+    '/blog',
+    'https://cdn.staticfile.org/amazeui/2.7.2/css/amazeui.min.css'
+]
+
+self.addEventListener('install',function(e){
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache){
+            return cache.addAll(filesCache)
+        })
+    )
+})
+
+self.addEventListener('activate', function(event) {
+
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(cacheNames.map(function(key) {
+                if( key !== cacheName) {
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+    return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+});
